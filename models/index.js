52585cb8c6684decd9fe569e670e8522044327e8
Kris Pennimpede <kpennimpede@hotmail.com>
2021-03-18 22:03:14 -0400

    Create models for User, Team, Player and Goalie. Updated 'Models' index.js and started assocaitions

--- models/index.js
+++ models/index.js
@@ -1,14 +1,32 @@
 // HUB TO ACCESS ALL MODELS
 // example:
 
-// const User = require("./User");
-// const Teams = require("./Teams");
-// const Player = require("./Player");
-// const Goalie = require("./Goalie");
+const User = require("./User");
+const Team = require("./Team");
+const Player = require("./Player");
+const Goalie = require("./Goalie");
+
+//User to Team association
+User.hasOne(Team, {
+  foreignKey: 'user_id'
+});
+
+Team.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+//Player to Team Associations
+
+Team.hasMany(Player);
+
+Player.belongsTo(Team, {
+});
+
+//Goalie associations
+Team.hasOne(Goalie);
+
+Goalie.belongsTo(Team);
 
 
 
-// ASSOCIATIONS
-
-// GO HERE!
-
+module.exports = { User, Team, Player, Goalie };
