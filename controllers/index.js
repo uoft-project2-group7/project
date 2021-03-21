a8970dc2f43587270aad936039347194967bb032
TerrenceJChan <terrencechan.writer@gmail.com>
2021-03-21 14:06:36 -0400

    Start on home-routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
+const homeRoutes = require('./home-routes.js');
 
 router.use((req, res) => {
-    res.status(404).end();
-  });
+  res.status(404).end();
+});
+
+router.use('/', homeRoutes);
 
 module.exports = router;
