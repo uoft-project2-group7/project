9d78627bf47e54cd9a440ef6f7bc0d11685510ff
Terrence Chan <terrencechan.writer@gmail.com>
2021-03-21 16:55:30 -0400

    Add seeds and routing information for homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,44 +4,33 @@ const { Player, Team, TeamPlayers, User } = require('../models');
 
 // get team cards for homepage
 
-// router.get('/', (req, res) => {
-//   console.log('======================');
-//   Post.findAll({
-//     attributes: [
-//       'id',
-//       'post_url',
-//       'title',
-//       'created_at',
-//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//     ],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//         include: {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['username']
-//       }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       const posts = dbPostData.map(post => post.get({ plain: true }));
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'username'
+    ],
+    include: [
+      {
+        model: Team,
+        attributes: ['id', 'team_name', 'center', 'user_id']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const teams = dbPostData.map(post => post.get({ plain: true }));
 
-//       res.render('homepage', {
-//         posts,
-//         loggedIn: req.session.loggedIn
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+      res.render('home', {
+        teams,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // get six random teams
 router.get('/team/:id', (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,13 @@
 const seedPlayers = require('./player-seed');
+const seedUsers = require('./user-seed');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  await seedUsers();
+  console.log('\n----- PLAYER DATA SEEDED -----\n');
   await seedPlayers();
   console.log('\n----- PLAYER DATA SEEDED -----\n');
 
