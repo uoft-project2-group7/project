32d17b029ffef9d54cd8befdaa2a0d4b161521ed
Terrence Chan <terrencechan.writer@gmail.com>
2021-03-22 23:08:50 -0400

    Selecting a player hides row and displays the next line

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -18,11 +18,44 @@ const nameFormHandler = () => {
     document.querySelector('#name-form').style.display = "none";
     stepNum++;
     printInstructions(stepNum);
+    document.querySelector('#c-selection').style.display = "grid";
+
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', cardSelectHandler);
+    }
+}
+
+const cardSelectHandler = () => {
+    alert('it worked');
+    if (document.getElementById('c-selection').style.display === "grid") {
+        document.getElementById('c-selection').style.display = "none";
+        document.getElementById('rw-selection').style.display = "grid"
+    } else if (document.getElementById('rw-selection').style.display === "grid") {
+        document.getElementById('rw-selection').style.display = "none";
+        document.getElementById('lw-selection').style.display = "grid"
+    } else if (document.getElementById('lw-selection').style.display === "grid") {
+        document.getElementById('lw-selection').style.display = "none";
+        document.getElementById('d1-selection').style.display = "grid"
+    } else if (document.getElementById('d1-selection').style.display === "grid") {
+        document.getElementById('d1-selection').style.display = "none";
+        document.getElementById('d2-selection').style.display = "grid"
+    } else if (document.getElementById('d2-selection').style.display === "grid") {
+        document.getElementById('d2-selection').style.display = "none";
+        document.getElementById('g-selection').style.display = "grid"
+    } else {
+        postData();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     printInstructions(stepNum);
+    document.querySelector('#c-selection').style.display = "none";
+    document.querySelector('#rw-selection').style.display = "none";
+    document.querySelector('#lw-selection').style.display = "none";
+    document.querySelector('#d1-selection').style.display = "none";
+    document.querySelector('#d2-selection').style.display = "none";
+    document.querySelector('#g-selection').style.display = "none";
 });
 
 document.querySelector('#submit-name').addEventListener('click', nameFormHandler);
-console.log('banana');
