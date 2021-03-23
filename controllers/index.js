58cabc5aa68d8a8f3db935f39c4434f25904fcb5
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-03-23 16:22:26 -0400

    first player

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require('./home-routes.js');
+const playerRoutes = require('./player-routes.js')
 // const displayRoute= require("./display")
 
 // router.use("/", displayRoute)
 router.use('/', homeRoutes);
 router.use("/api", apiRoutes);
+router.use('/', playerRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
