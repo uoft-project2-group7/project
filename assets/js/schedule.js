819a05558abec61f312e864850915394a53a500b
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-03-25 22:17:42 -0400

    Current games and scores

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -3,10 +3,23 @@ var getNhlSchedule = function() {
   const apiUrl = "https://statsapi.web.nhl.com/api/v1/schedule";
   fetch(apiUrl).then(function(response){
   response.json().then(function(data) {
-    let totalgames = data.dates[0].games[2].teams.away;
-    
+    let totalgames = data.dates[0].games[2].teams;
+    let awayteam = totalgames.away.team.name; 
+    let hometeam = totalgames.home.team.name;
+    let scoreaway = totalgames.away.score;
+    let scorehome = totalgames.home.score;
+
     console.log(totalgames);
+    console.log(awayteam);
+    console.log(hometeam);
+    console.log(scoreaway);
+    console.log(scorehome);
+    document.getElementById('future1').innerHTML = awayteam;
   }) 
+
+  .catch((err) => {
+    console.log(err);
+  })
     
   });
   
